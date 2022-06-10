0b6b4ea12220a80264ec93e7a81b203b33b8a4ea
Sherly-Ann Tapil <sherly-annstapil@iskolarngbayan.pup.edu.ph>
2022-06-10 23:59:44 +0800

    my game

---
+++ tictactoe1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace tictactoe1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.D = new System.Windows.Forms.Button();
+            this.E = new System.Windows.Forms.Button();
+            this.F = new System.Windows.Forms.Button();
+            this.G = new System.Windows.Forms.Button();
+            this.H = new System.Windows.Forms.Button();
+            this.I = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.BackColor = System.Drawing.SystemColors.Menu;
+            this.A.Location = new System.Drawing.Point(97, 56);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(97, 73);
+            this.A.TabIndex = 0;
+            this.A.UseVisualStyleBackColor = false;
+            this.A.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // B
+            // 
+            this.B.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.B.Location = new System.Drawing.Point(97, 156);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(97, 73);
+            this.B.TabIndex = 1;
+            this.B.UseVisualStyleBackColor = false;
+            this.B.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.C.Location = new System.Drawing.Point(97, 253);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(97, 73);
+            this.C.TabIndex = 2;
+            this.C.UseVisualStyleBackColor = false;
+            this.C.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // D
+            // 
+            this.D.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.D.Location = new System.Drawing.Point(222, 56);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(97, 73);
+            this.D.TabIndex = 3;
+            this.D.UseVisualStyleBackColor = false;
+            this.D.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // E
+            // 
+            this.E.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.E.Location = new System.Drawing.Point(222, 156);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(97, 73);
+            this.E.TabIndex = 4;
+            this.E.UseVisualStyleBackColor = false;
+            this.E.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // F
+            // 
+            this.F.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.F.Location = new System.Drawing.Point(222, 253);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(97, 73);
+            this.F.TabIndex = 5;
+            this.F.UseVisualStyleBackColor = false;
+            this.F.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // G
+            // 
+            this.G.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.G.Location = new System.Drawing.Point(352, 56);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(97, 73);
+            this.G.TabIndex = 6;
+            this.G.UseVisualStyleBackColor = false;
+            this.G.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // H
+            // 
+            this.H.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.H.Location = new System.Drawing.Point(352, 156);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(97, 73);
+            this.H.TabIndex = 7;
+            this.H.UseVisualStyleBackColor = false;
+            this.H.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // I
+            // 
+            this.I.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.I.Location = new System.Drawing.Point(352, 253);
+            this.I.Name = "I";
+            this.I.Size = new System.Drawing.Size(97, 73);
+            this.I.TabIndex = 8;
+            this.I.UseVisualStyleBackColor = false;
+            this.I.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.I);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.G);
+            this.Controls.Add(this.F);
+            this.Controls.Add(this.E);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.Button E;
+        private System.Windows.Forms.Button F;
+        private System.Windows.Forms.Button G;
+        private System.Windows.Forms.Button H;
+        private System.Windows.Forms.Button I;
+    }
+}
+
---
+++ tictactoe1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int players = 2; //even = 0 move; odd = odd = X move
+        public int move = 0; //counting moves;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (players % 2 != 0)
+            {
+                button.Text = "O";
+                players++;
+                move++;
+            }
+            else
+            {
+                button.Text = "X";
+                players++;
+                move++;
+            }
+
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X wins!");
+                    Close();
+                }
+                else
+                {
+                    button.Text = "X";
+                    players++;
+                    move++;
+                }
+
+                if (CheckWinner() == true)
+                {
+                    if (button.Text == "X")
+                    {
+                        MessageBox.Show("X wins!");
+                        Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("O wins!");
+                        Close();
+                    }
+
+                    if (CheckDraw() == true)
+                    {
+                        MessageBox.Show("Issa draw!");
+                        Close();
+                    }
+                }
+                bool CheckWinner()
+                {
+                    //horizontal checks
+                    if ((A.Text == D.Text) && (D.Text == G.Text) && A.Text != "")
+                        return true;
+                    else if ((B.Text == E.Text) && (E.Text == H.Text) && B.Text != "")
+                        return true;
+                    else if ((C.Text == F.Text) && (F.Text == I.Text) && C.Text != "")
+                        return true;
+
+                    //vertical checks
+                    if ((A.Text == B.Text) && (B.Text == C.Text) && A.Text != "")
+                        return true;
+                    else if ((D.Text == E.Text) && (E.Text == F.Text) && D.Text != "")
+                        return true;
+                    else if ((G.Text == H.Text) && (H.Text == I.Text) && G.Text != "")
+                        return true;
+
+                    //diagonal checks
+                    if ((A.Text == E.Text) && (E.Text == I.Text) && A.Text != "")
+                        return true;
+                    else if ((G.Text == E.Text) && (E.Text == C.Text) && G.Text != "")
+                        return true;
+                    else
+                        return false;
+                }
+
+                bool CheckDraw()
+                {
+                    if (move == 9)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            {
+                this.Close();
+            }
+        }
+
+        private bool CheckWinner()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
